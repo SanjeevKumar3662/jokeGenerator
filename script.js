2f6175750fde18c8304c46555bab694ca8c9a5e1
HellSaw-Ubuntu <sanjeevkumar3662work@gmail.com>
2025-03-05 19:13:12 +0530

    get another joke btn will now work

--- script.js
+++ script.js
@@ -6,8 +6,6 @@ xhr.onreadystatechange = function () {
   if (this.readyState === 4 && this.status === 200) {
     const data = JSON.parse(this.responseText);
     const oldJoke = document.querySelector("#joke");
-    console.log(oldJoke);
-    console.log(data);
 
     const jokeText = document.createTextNode(data.value);
     oldJoke.firstChild.replaceWith(jokeText);
@@ -15,3 +13,11 @@ xhr.onreadystatechange = function () {
 };
 
 xhr.send();
+
+function sendRequest() {
+  xhr.open("GET", "https://api.chucknorris.io/jokes/random");
+
+  xhr.send();
+}
+const btn = document.querySelector("#joke-btn");
+btn.addEventListener("click", sendRequest);

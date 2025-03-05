ad01ddd58fb257482da64bb6303a4a6fac5671db
HellSaw-Ubuntu <sanjeevkumar3662work@gmail.com>
2025-03-05 19:23:37 +0530

    rearranged code into a getJoke funciton

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
 const xhr = new XMLHttpRequest();
 
-xhr.open("GET", "https://api.chucknorris.io/jokes/random");
-
-xhr.onreadystatechange = function () {
-  if (this.readyState === 4 && this.status === 200) {
-    const data = JSON.parse(this.responseText);
-    const oldJoke = document.querySelector("#joke");
-
-    const jokeText = document.createTextNode(data.value);
-    oldJoke.firstChild.replaceWith(jokeText);
-  }
-};
-
-xhr.send();
-
-function sendRequest() {
+function getJoke() {
   xhr.open("GET", "https://api.chucknorris.io/jokes/random");
 
+  xhr.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+      const data = JSON.parse(this.responseText);
+      const oldJoke = document.querySelector("#joke");
+
+      const jokeText = document.createTextNode(data.value);
+      oldJoke.firstChild.replaceWith(jokeText);
+    }
+  };
   xhr.send();
 }
+
+getJoke();
+
 const btn = document.querySelector("#joke-btn");
-btn.addEventListener("click", sendRequest);
+btn.addEventListener("click", getJoke);
